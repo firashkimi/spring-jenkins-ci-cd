d1a5b2f9bd6ffbc19d6c662e4da689c5c95ae1bf
hkimifiras <firashkimi709@gmail.com>
2024-08-07 09:56:43 +0100

    first commit

---
+++ src/main/java/tn/firas/devopscicd/DevopsCiCdApplication.java
@@ -0,0 +1,13 @@
+package tn.firas.devopscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsCiCdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/firas/devopscicd/DevopsCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.firas.devopscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
